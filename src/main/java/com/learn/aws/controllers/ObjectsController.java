08864ae0f499e31131e41a2bac5d2d98c804d085
Danish Javed <danishjaved636@gmail.com>
2022-08-09 18:44:38 +0530

    Update ObjectsController.java

--- src/main/java/com/learn/aws/controllers/ObjectsController.java
+++ src/main/java/com/learn/aws/controllers/ObjectsController.java
@@ -4,12 +4,10 @@ import com.amazonaws.AmazonServiceException;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.model.*;
 import com.amazonaws.util.IOUtils;
 import com.learn.aws.utils.Utils;
+import org.apache.commons.logging.impl.AvalonLogger;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -18,12 +16,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("objects")
 public class ObjectsController {
     final AmazonS3 s3;
@@ -46,68 +49,36 @@ public class ObjectsController {
     }
 
     @GetMapping("download/{bucketName}/{key}")
-    public ResponseEntity downloadObject(@PathVariable String bucketName, @PathVariable String key){
+    public ResponseEntity getObject(@PathVariable String bucketName, @PathVariable String key) throws IOException {
         S3Object object= s3.getObject(bucketName,key);
-        S3ObjectInputStream s3ObjectInputStream= object.getObjectContent();
-        FileOutputStream fileOutputStream;
-        File file=new File(key);
-        Path path = Paths.get(".\\Downloaded\\"+file.getName());
-        try {
-            fileOutputStream =new FileOutputStream(file);
-            byte[] read_buff= new byte[1024];
-            int read_len=0;
-            while ((read_len=s3ObjectInputStream.read(read_buff))>0){
-                fileOutputStream.write(read_buff);
-            }
-            fileOutputStream.close();
-            InputStreamResource inputStreamResource= new InputStreamResource(new FileInputStream(file));
-            HttpHeaders header = new HttpHeaders();
-            header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + key);
-            header.add("Cache-Control", "no-cache, no-store, must-revalidate");
-            header.add("Pragma", "no-cache");
-            header.add("Expires", "0");
-
-            return ResponseEntity.ok()
-                    .headers(header)
-                    .contentLength(file.length())
-                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                    .body(inputStreamResource);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
+        byte[] byteArray= IOUtils.toByteArray(object.getObjectContent());
+        return ResponseEntity.ok(byteArray);
     }
 
-    @GetMapping("getImage/{bucketName}/{key}")
-    public ResponseEntity getObject(@PathVariable String bucketName, @PathVariable String key){
-        S3Object object= s3.getObject(bucketName,key);
-        S3ObjectInputStream s3ObjectInputStream= object.getObjectContent();
-        FileOutputStream fileOutputStream;
-        File file=new File(key);
-        Path path = Paths.get(".\\Downloaded\\"+file.getName());
-        try {
-            S3Object o = s3.getObject(bucketName, key);
-            S3ObjectInputStream s3is = o.getObjectContent();
-            FileOutputStream fos = new FileOutputStream(file);
-            byte[] read_buf = new byte[1024];
-            int read_len = 0;
-            while ((read_len = s3is.read(read_buf)) > 0) {
-                fos.write(read_buf, 0, read_len);
+    @GetMapping("listObjectNames/{bucketName}")
+    public ResponseEntity getObjectNameList(@PathVariable String bucketName){
+       List<String> objectNames=new ArrayList<>();
+       s3.listObjectsV2(bucketName).getObjectSummaries().forEach(object->objectNames.add(object.getKey()));
+       return ResponseEntity.ok(objectNames);
+    }
+
+    @GetMapping("getObjectsLink/{bucketName}/{directoryName}")
+    public ResponseEntity getLinks(@PathVariable String bucketName, @PathVariable String directoryName){
+       return ResponseEntity.ok( s3.getUrl(bucketName,directoryName));
+    }
+
+    @GetMapping("getImages/{bucketName}/{directory}")
+    public ResponseEntity getImages(@PathVariable String bucketName,@PathVariable String directory){
+        List<byte[]> images=new ArrayList<>();
+        List<String> imagesNames=new ArrayList<>();
+        s3.listObjects(bucketName,directory).getObjectSummaries().forEach(object->imagesNames.add(object.getKey()));
+        imagesNames.forEach(imageName->{
+            try {
+                images.add(IOUtils.toByteArray(s3.getObject(bucketName,imageName).getObjectContent()));
+            } catch (IOException e) {
+
             }
-            s3is.close();
-            fos.close();
-            return ResponseEntity.ok(new InputStreamResource(new FileInputStream(file)));
-        } catch (AmazonServiceException e) {
-            System.err.println(e.getErrorMessage());
-            return null;
-        } catch (FileNotFoundException e) {
-            System.err.println(e.getMessage());
-            return null;
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-            return null;
-        }
+        });
+        return ResponseEntity.ok(images);
     }
 }
